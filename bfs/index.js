e89ee5d1a5093e1815e14569ab4c0ae96075231b
Flow-Fly <florian.aube@gmail.com>
2024-03-13 15:48:13 +0100

    Adde some comments

--- bfs/index.js
+++ bfs/index.js
@@ -6,6 +6,10 @@ startButton.addEventListener("click", generateCells)
 
 let usedAlgo = null
 
+/**
+ * The Node class receives an HTML element as argument.
+ * It will start as being unvisited, this is important for later.
+ */
 class Node {
 	constructor(element) {
 		this.element = element
@@ -13,6 +17,11 @@ class Node {
 		this.className = "wall"
 		this.goal = false
 	}
+
+	/**
+	 * Using getter and setter.
+	 * Those are handy to hide away some behaviours (look at the type setter)
+	 */
 
 	get position() {
 		return this.coordinates
@@ -87,12 +96,15 @@ async function generateCells() {
 		updateText(`Generating row ${i + 1}`)
 		for (let j = 0; j < 20; j++) {
 			const div = document.createElement("div")
+			// All cells starts as "walls" that will need to be explored
 			div.classList.add("wall", "cell")
 			mazeContainer.append(div)
+			// We then create a node instance, pushing it into a subArray and then in the matrice.
 			const node = new Node(div)
 			node.type = "wall"
 			node.position = { x: i, y: j }
 			subArray.push(node)
+			// Little bit of sleeping here in order to have some visualisation
 			await sleep(10)
 		}
 		matrice.push(subArray)
@@ -101,6 +113,7 @@ async function generateCells() {
 	const randomFinish =
 		matrice[Math.floor(Math.random() * 20)][Math.floor(Math.random() * 20)]
 	randomFinish.goal = true
+	// Get a random finish. This is totally unecessary.
 
 	updateText("generated all the cells...")
 	await sleep(1000)
@@ -117,24 +130,40 @@ async function generateCells() {
 	updateText("Starting.")
 	await sleep(200)
 	generateMaze(matrice)
+	// A lot of fanciness for no reasons.
 }
 
 async function generateMaze(matrice) {
 	const row = 0
 	const col = 0
+	/**
+	 * In DFS we use something called a stack with a LIFO order
+	 * LIFO: Last In, First Out. (think of a crowded subway, you're the last in: you're the first out.)
+	 * We push in the stack and then get the last element, leading to LIFO.
+	 *
+	 * In BFS, we use a FIFO order
+	 * FIFO: First In, First Out
+	 */
 	const stack = []
 	const startingPoint = matrice[row][col]
+
+	// Add the starting point to our stack and visit it.
 	stack.push(startingPoint)
 	startingPoint.isVisited = true
 	startingPoint.type = "start"
 	updateText("exploring")
+	// We will run as long as there is something to do.
 	while (stack.length > 0) {
+		// Here we switch between the FIFO or LIFO order using shift / pop
 		const currentNode = usedAlgo === "bfs" ? stack.shift() : stack.pop()
+		// Get all the valid neighbor of the current node
+		// Out of the matrice is unvalid and already visited are not valid aswell.
 		const neighbors = getUnvisitedNeighbors(matrice, currentNode)
 		// console.log(neighbors)
 		if (neighbors.length > 0) {
 			// push back the current node in stack if it has neighbors
 			stack.push(currentNode)
+			// Get a random node from the neighbors
 			const rand = Math.floor(Math.random() * neighbors.length)
 			const { cell: nextNode, direction } = neighbors[rand]
 			currentNode.removeBorder(direction)
